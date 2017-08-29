0be3672969dfbffdee2e89d0cbdf09c32c65087f
Aresthor <aresthor@AresthordeMac-mini.local>
2017-08-29 15:18:27 +0800

    定义模型

---
+++ iOS_Dome1/iOS_Dome1/Person.swift
@@ -0,0 +1,16 @@
+//
+//  Person.swift
+//  iOS_Dome1
+//
+//  Created by Aresthor on 2017/8/29.
+//  Copyright © 2017年 Aresthor. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name:String?
+    var phone:String?
+    var title:String?
+
+}
