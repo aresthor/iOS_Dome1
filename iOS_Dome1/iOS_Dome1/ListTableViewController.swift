8c055bee558ae2740c03933a24d8acd9418adf23
Aresthor <aresthor@AresthordeMac-mini.local>
2017-08-29 15:36:38 +0800

    异步加载数据，回调闭包

--- iOS_Dome1/iOS_Dome1/ListTableViewController.swift
+++ iOS_Dome1/iOS_Dome1/ListTableViewController.swift
@@ -9,20 +9,47 @@
 import UIKit
 
 class ListTableViewController: UITableViewController {
+    
+    var personList=[Person]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        loadData{(list) in
+            print(list)
+            self.personList+=list
+            
+            self.tableView.reloadData()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func loadData(completion:@escaping (_ list:[Person])->())->(){
+        
+        DispatchQueue.global().async {
+            print("正在努力加载中...")
+            Thread.sleep(forTimeInterval: 1)
+            
+            var arrayM=[Person]()
+            
+            for i in 0..<20{
+                let p=Person()
+                
+                p.name="ZhangSan-\(i)"
+                p.phone="1860"+String(format: "%06D", arc4random_uniform(100000))
+                p.title="Boss"
+                
+                arrayM.append(p)
+            }
+            
+            DispatchQueue.main.async(execute: {
+                completion(arrayM)
+            })
+        }
     }
 
   
