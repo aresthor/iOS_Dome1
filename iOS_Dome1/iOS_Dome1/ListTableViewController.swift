7cdc72ac3b9c24b26b2bd8b245c06e8f87500bde
Aresthor <aresthor@AresthordeMac-mini.local>
2017-08-29 15:08:02 +0800

    界面搭建

---
+++ iOS_Dome1/iOS_Dome1/ListTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListTableViewController.swift
+//  iOS_Dome1
+//
+//  Created by Aresthor on 2017/8/29.
+//  Copyright © 2017年 Aresthor. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+  
+}
